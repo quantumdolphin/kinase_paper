529fd852a7e798e3aaadcb45c07143aa2a2b9ec6
amithvikram rangarajan <39220547+quantumdolphin@users.noreply.github.com>
2022-09-08 16:51:51 -0700

    identify PDB structures and download them from PDB renum server
    # first part of the code uses biotite to search for suitable PDB structures with uniprot id as input.
    # second part is a simple python script that takes a list of pdbids and downloads the corresponding structures from PDB renum server.
    # The downloaded PDB structures will be in pdb.gz format , which is read both by chimerax and biopandas

---
+++ get_structures.py
@@ -0,0 +1,26 @@
+import biotite as bt
+import biotite.database.rcsb as rcsb
+import requests
+import os
+import gzip
+import io
+import shutil
+###########
+query1 = rcsb.FieldQuery("rcsb_polymer_entity_container_identifiers.reference_sequence_identifiers.database_accession", exact_match ='P07333')
+query2 = rcsb.FieldQuery("reflns.d_resolution_high", less=2.5)
+final_query = query1 & query2
+pdb_ids = rcsb.search(final_query)
+#################
+for id in pdb_ids:
+    download_url_prefix = 'http://dunbrack3.fccc.edu/PDBrenum/output_PDB/'
+    download_url_suffix = '_renum.pdb.gz'
+    pdb_id = id.lower()
+    print(pdb_id)
+    download_url = download_url_prefix + pdb_id + download_url_suffix
+    print(download_url)
+    response = requests.get(download_url)
+    print(response.status_code)
+    fname = pdb_id + '.pdb.gz'
+    with open(fname, 'wb') as f:
+        f.write(response.content)
+    print(f'completed writing {fname}..')
